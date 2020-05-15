5afa629e936394b61108c22f37f9ed89b9f8127f
kdabu00 <55458444+kdabu00@users.noreply.github.com>
2020-05-15 14:12:19 -0700

    modified tests

--- test/test.js
+++ test/test.js
@@ -3,63 +3,63 @@ const jsdom = require('mocha-jsdom');
 
 global.document = jsdom({url: "http://localhost"});
 
-describe('Test', function(){ 
+describe('Test', function(){    
   
   describe('# Initial Basic Values', function(){
     it('Initial count value 0', function(){
       var myCode = require('../proj.js');
-      expect(myCode.count).to.equal(undefined);
+      expect(myCode.count).to.equal(0);
     });
     it('Initial score value 0', function(){
       var myCode = require('../proj.js');
-      expect(myCode.score).to.equal(undefined);
+      expect(myCode.score).to.equal(0);
     });
     it('Initial highscroe value 0', function(){
       var myCode = require('../proj.js');
-      expect(myCode.highscore).to.equal(undefined);
+      expect(myCode.highscore).to.equal(0);
     });
     it('Initial speed value 2000', function(){
       var myCode = require('../proj.js');
-      expect(myCode.speed).to.equal(undefined);
+      expect(myCode.speed).to.equal(2000);
     });
 
     it('Initial count value type', function(){
-//       var myCode = require('../proj.js');
-//       expect(myCode.count).to.be.a(undefined);
+      var myCode = require('../proj.js');
+      expect(myCode.count).to.be.a('number');
     });
     it('Initial score value type', function(){
-//       var myCode = require('../proj.js');
-//       expect(myCode.score).to.be.a(undefined);
+      var myCode = require('../proj.js');
+      expect(myCode.score).to.be.a('number');
     });
     it('Initial highscore value type', function(){
-//       var myCode = require('../proj.js');
-//       expect(myCode.highscore).to.be.a(undefined);
+      var myCode = require('../proj.js');
+      expect(myCode.highscore).to.be.a('number');
     });
     it('Initial speed value type', function(){
-//       var myCode = require('../proj.js');
-//       expect(myCode.speed).to.be.a(undefined);
+      var myCode = require('../proj.js');
+      expect(myCode.speed).to.be.a('number');
     });
 
     it('Initial scoreboard type', function(){
-//       var myCode = require('../proj.js');
-//       expect(myCode.scoreboard).to.be.a(undefined);
+      var myCode = require('../proj.js');
+      expect(myCode.scoreboard).to.be.a('array');
     });
 
     it('Initial survival type', function(){
       var myCode = require('../proj.js');
-      expect(myCode.survival).to.be.undefined;
+      expect(myCode.survival).to.be.false;
     });
     it('Initial insane type', function(){
       var myCode = require('../proj.js');
-      expect(myCode.insane).to.be.undefined;
+      expect(myCode.insane).to.be.false;
     });
     it('Initial lives value 10', function(){
       var myCode = require('../proj.js');
-      expect(myCode.lives).to.equal(undefined);
+      expect(myCode.lives).to.equal(10);
     });
     it('Initial lives value type', function(){
-//       var myCode = require('../proj.js');
-//       expect(myCode.lives).to.be.a(undefined);
+      var myCode = require('../proj.js');
+      expect(myCode.lives).to.be.a('number');
     });
 
   });
@@ -67,12 +67,12 @@ describe('Test', function(){
   describe('# Initial Timer Values', function(){
     it('initial time value', function(){
       var myCode = require('../proj.js');
-      expect(myCode.time).to.equal(undefined);
+      expect(myCode.time).to.equal(0);
     });
     
     it('Number initial time type', function(){
-//       var myCode = require('../proj.js');
-//       expect(myCode.time).to.be.a(undefined);
+      var myCode = require('../proj.js');
+      expect(myCode.time).to.be.a('number');
     });
     
     it('undefined timer1-4 values', function(){
@@ -93,9 +93,9 @@ describe('Test', function(){
 
     it('Boolean two, three, four values', function(){
       var myCode = require('../proj.js');
-      expect(myCode.two).to.be.undefined;
-      expect(myCode.three).to.be.undefined;
-      expect(myCode.four).to.be.undefined;
+      expect(myCode.two).to.be.false;
+      expect(myCode.three).to.be.false;
+      expect(myCode.four).to.be.false;
     });
 
     it('undefined GameTimer', function(){
@@ -105,103 +105,95 @@ describe('Test', function(){
   });
 
   describe('# Check Functions', function() {
-//     it('letsRandomize() should be a number', function() {
-//       var myCode = require('../proj.js');
-//       let result = myCode.letsRandomize();
-//       expect(result).to.be.a(undefined);
-//     });
-
-//     it('molePlacer1-4()');
-
-//     it('moveClicked1-4()', function() {
-//       var myCode = require('../proj.js');
-//       let w = myCode.letsRandomize(0, (window.innerWidth - 200));
-//       let h = myCode.letsRandomize(0, (window.innerHeight - 200));
-//       boom1display = document.getElementById('boom1').style.display;
-//       boom1display = "inline-block";
-
-//       expect(boom1display).to.be.a('string');
-//     });
-
-//     it('moleMove()', function(){
-//       var myCode = require('../proj.js');
-//       let w = myCode.letsRandomize(0, (window.innerWidth - 200));
-//       let h = myCode.letsRandomize(0, (window.innerHeight - 200));
-//       mole1top = document.getElementById('mole1').style.top;
-//       mole1top = h +'px';
-//       mole1left = document.getElementById('mole1').style.left;
-//       mole1left = w +'px';
-
-//       boom1top = document.getElementById('boom1').style.top;
-//       boom1top = h +'px';
-//       boom1left = document.getElementById('boom1').style.left;
-//       boom1left = w +'px';
-
-//       expect(mole1top).to.be.a(undefined);
-//       expect(mole1left).to.be.a(undefined);
-//       expect(boom1top).to.be.a(undefined);
-//       expect(boom1left).to.be.a(undefined);
-//     } );
-
-//     it('showM1-4()', function() {
-//       var myCode = require('../proj.js');
-//       let w = myCode.letsRandomize(0, (window.innerWidth - 200));
-//       let h = myCode.letsRandomize(0, (window.innerHeight - 200));
-//       mole1top = document.getElementById('mole1').style.top;
-//       mole1top = h +'px';
-//       mole1left = document.getElementById('mole1').style.left;
-//       mole1left = w +'px';
-
-//       boom1top = document.getElementById('boom1').style.top;
-//       boom1top = h +'px';
-//       boom1left = document.getElementById('boom1').style.left;
-//       boom1left = w +'px';
-
-//       expect(mole1top).to.be.a('string');
-//       expect(mole1left).to.be.a('string');
-//       expect(boom1top).to.be.a('string');
-//       expect(boom1left).to.be.a('string');
-
-//       boom1display = document.getElementById('boom1').style.display;
-//       boom1display = "inline-block";
-
-//       expect(boom1display).to.be.a('string');
-//     } );
-
-//     it('showMole()', function() {
-//       mole1display = document.getElementById('mole1').style.display;
-//       mole1display = "inline-block";
-//       boom1display = document.getElementById('boom1').style.display;
-//       boom1display = "none";
-
-//       expect(mole1display).to.be.a('string');
-//       expect(boom1display).to.be.a('string');
-//     });
-
-//     it('animate()', function() {
-//       mole1display = document.getElementById('mole1').style.display;
-//       mole1display = "none";
-//       boom1display = document.getElementById('boom1').style.display;
-//       boom1display = "inline-block";
-
-//       expect(mole1display).to.be.a('string');
-//       expect(boom1display).to.be.a('string');
-//     });
-
-    it('gameStart()', function() {
-    
+    it('letsRandomize() should be a number', function() {
+      var myCode = require('../proj.js');
+      let result = myCode.letsRandomize();
+      expect(result).to.be.a('number');
     });
 
-    it('survivalMode()', function() {
-    
+    it('molePlacer1-4()');
+
+    it('moveClicked1-4()', function() {
+      var myCode = require('../proj.js');
+      let w = myCode.letsRandomize(0, (window.innerWidth - 200));
+      let h = myCode.letsRandomize(0, (window.innerHeight - 200));
+      boom1display = document.getElementById('boom1').style.display;
+      boom1display = "inline-block";
+
+      expect(boom1display).to.be.a('string');
     });
 
-    it('insaneMode()', function() {
-    
+    it('moleMove()', function(){
+      var myCode = require('../proj.js');
+      let w = myCode.letsRandomize(0, (window.innerWidth - 200));
+      let h = myCode.letsRandomize(0, (window.innerHeight - 200));
+      mole1top = document.getElementById('mole1').style.top;
+      mole1top = h +'px';
+      mole1left = document.getElementById('mole1').style.left;
+      mole1left = w +'px';
+
+      boom1top = document.getElementById('boom1').style.top;
+      boom1top = h +'px';
+      boom1left = document.getElementById('boom1').style.left;
+      boom1left = w +'px';
+
+      expect(mole1top).to.be.a('string');
+      expect(mole1left).to.be.a('string');
+      expect(boom1top).to.be.a('string');
+      expect(boom1left).to.be.a('string');
+    } );
+
+    it('showM1-4()', function() {
+      var myCode = require('../proj.js');
+      let w = myCode.letsRandomize(0, (window.innerWidth - 200));
+      let h = myCode.letsRandomize(0, (window.innerHeight - 200));
+      mole1top = document.getElementById('mole1').style.top;
+      mole1top = h +'px';
+      mole1left = document.getElementById('mole1').style.left;
+      mole1left = w +'px';
+
+      boom1top = document.getElementById('boom1').style.top;
+      boom1top = h +'px';
+      boom1left = document.getElementById('boom1').style.left;
+      boom1left = w +'px';
+
+      expect(mole1top).to.be.a('string');
+      expect(mole1left).to.be.a('string');
+      expect(boom1top).to.be.a('string');
+      expect(boom1left).to.be.a('string');
+
+      boom1display = document.getElementById('boom1').style.display;
+      boom1display = "inline-block";
+
+      expect(boom1display).to.be.a('string');
+    } );
+
+    it('showMole()', function() {
+      mole1display = document.getElementById('mole1').style.display;
+      mole1display = "inline-block";
+      boom1display = document.getElementById('boom1').style.display;
+      boom1display = "none";
+
+      expect(mole1display).to.be.a('string');
+      expect(boom1display).to.be.a('string');
     });
 
-    it('handler()', function() {
-    
+    it('animate()', function() {
+      mole1display = document.getElementById('mole1').style.display;
+      mole1display = "none";
+      boom1display = document.getElementById('boom1').style.display;
+      boom1display = "inline-block";
+
+      expect(mole1display).to.be.a('string');
+      expect(boom1display).to.be.a('string');
     });
+
+    it('gameStart()');
+
+    it('survivalMode()');
+
+    it('insaneMode()');
+
+    it('handler()');
   });
 })
